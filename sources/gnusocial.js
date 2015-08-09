95ddee105eb30753fe177947fc0358e095a838cc
Chimo <chimo@chromic.org>
2015-08-08 22:47:56 -0400

    gnusocial: convert remaining AS obj-types to atom
    TODO: The parsing in the switch/case is getting pretty convoluted
    (XML...). It should be moved to methods, at the very least.

--- sources/gnusocial.js
+++ sources/gnusocial.js
@@ -14,6 +14,7 @@
             item = doc.getElementsByTagName( "entry" )[ 0 ],
             by,
             activityNS = "http://activitystrea.ms/spec/1.0/",
+            pocoNS = "http://portablecontacts.net/spec/1.0",
             event = {};
 
         event.published = item.getElementsByTagName( "published" )[ 0 ].textContent;
@@ -28,8 +29,16 @@
                 event.content = item.getElementsByTagNameNS( activityNS, "object" )[ 0 ].getElementsByTagName( "content" )[ 0 ].textContent;
                 break;
             case "share": /* repeat */
-                event.title = "Repeated a " + event.objectType;
-                event.content = item.content;
+                event.title = "Repeated a " +
+                    activityRE.exec(
+                        item
+                            .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                            .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                            .getElementsByTagNameNS( activityNS, "object-type" )[ 0 ].textContent
+                    );
+                event.content = item
+                    .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                    .getElementsByTagName( "content" ).textContent;
                 break;
             case "post":
                 event.title = "Posted a " + event.objectType;
@@ -40,23 +49,45 @@
                     event.content = item.getElementsByTagName( "content" )[ 0 ].textContent;
                 }
                 break;
-            case "join": // TODO: Change to atom
+            case "join":
                 event.title = "Joined a " + event.objectType;
-                event.content = "<a href='" + item.object.url + "'>" +
-                    item.object.portablecontacts_net.displayName + "</a> " +
-                    "(" + item.object.portablecontacts_net.preferredUsername + ")<br />" +
-                    ( item.object.portablecontacts_net.note || "" );
+                event.content = "<a href='" +
+                    item
+                        .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                        .getElementsByTagName( "link" )[ 0 ]
+                        .getAttribute( "href" ) +
+                    "'>" +
+                    item.getElementsByTagNameNS( pocoNS, "displayName" )[ 0 ].textContent + "</a> " +
+                    "(" + item.getElementsByTagNameNS( pocoNS, "preferredUsername" )[ 0 ].textContent + ")<br />" +
+                    ( item.getElementsByTagNameNS( pocoNS, "note" )[ 0 ].textContent || "" );
                 break;
-            case "follow": // TODO: Change to atom
+            case "follow":
                 event.title = "Started following";
-                event.content = "<a class='h-card u-url' href='" + item.object.url + "'>" +
-                                    "<img class='u-photo' src='" + item.object.image.url + "' alt='" + item.object.displayName + "&#39;s avatar'>" +
-                                    "<span class='p-nickname'>" + item.object.displayName +
-                                "</span></a>";
+                event.content = "<a class='h-card u-url' href='" +
+                    item
+                        .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                        .getElementsByTagName( "link" )[ 0 ]
+                        .getAttribute( "href" ) +
+                    "'>" +
+                    "<img class='u-photo' src='" +
+                    item
+                        .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                        .getElementsByTagName( "link" )[ 1 ]
+                        .getAttribute( "href" ) +
+                    "' alt='" +
+                    item
+                        .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                        .getElementsByTagNameNS( pocoNS, "displayName" )[ 0 ].textContent +
+                    "&#39;s avatar'>" +
+                    "<span class='p-nickname'>" +
+                    item
+                        .getElementsByTagNameNS( activityNS, "object" )[ 0 ]
+                        .getElementsByTagNameNS( pocoNS, "displayName" )[ 0 ].textContent +
+                    "</span></a>";
                 break;
             default:
-                event.title = item.title;
-                event.content = item.content;
+                event.title = item.getElementsByTagName( "title" )[ 0 ].textContent;
+                event.content = item.getElementsByTagName( "content" )[ 0 ].textContent;
                 break;
         }
 
