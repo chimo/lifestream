8c8a7d2542cb0e0b32e2727e5925ee1b83c067b0
Chimo <chimo@chromic.org>
2015-09-06 11:38:39 -0400

    Revert "gogs: work-around upstream 'time-since' bug"
    This reverts commit 7fe916de667a315ac3be088db4e46b442f634de9.
    
    The upstream bug has been fixed as of 0.6.9

--- sources/gogs.js
+++ sources/gogs.js
@@ -52,8 +52,7 @@
             $latest = $( ".news" ).first(),
             $items = $latest.find( ".content li" ), // Commits + optional "compare" link
             $lastLink = $items.last().find( "a" ), // Either the last commit or the "compare" link
-            urlParts = urlParser.parse( subscription.topic ),
-            buggyDate, goodDate;
+            urlParts = urlParser.parse( subscription.topic );
 
         hostname = urlParts.protocol + "//" + urlParts.host;
 
@@ -63,13 +62,7 @@
         // If so, use the "href" as the source link. If not, blank url (for now)
         event.source = ( $lastLink.text().match( /^View comparison/ ) ) ? $lastLink.attr( "href" ) : "";
 
-        // Attempt to work-around gogs time-since bug
-        // https://github.com/gogits/gogs/issues/1500
-        // TODO: Remove once upstream fixes it
-        buggyDate = new Date( $( ".time-since" ).attr( "title" ) );
-        goodDate = new Date( buggyDate.getTime() + ( buggyDate.getTimezoneOffset() * 60 * 1000 ) );
-        event.published = goodDate.toISOString();
-
+        event.published = $( ".time-since" ).attr( "title" );
         event.content = getContent( $items, $ );
         event.objectType = "file"; // ?
         event.verb = "update"; // ?
